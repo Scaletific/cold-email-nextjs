0d49dab7688c6cf901404ab7b23e8c33e1bbacc7
Josephoduro <josephoduro895@gmail.com>
2024-09-24 12:29:25 +0000

    resolved eslint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,41 +1,39 @@
-import { Toaster } from "@/components/ui/sonner"
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Providers from "./components/Provider"
-import './globals.css'
-import { Suspense } from "react"
+import { Toaster } from "@/components/ui/sonner";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+// import Providers from "./components/Provider"
+import "./globals.css";
+import { Suspense } from "react";
 
+config.autoAddCss = false;
 
-config.autoAddCss = false
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Login',
-  description: 'Login Page Description',
+  title: "Login",
+  description: "Login Page Description",
   icons: {
-    icon: './favicon.ico',
+    icon: "./favicon.ico",
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-
   return (
     <html lang="en">
       <body className={inter.className}>
         <main>
           <Suspense>
-            <Providers>{children}</Providers>
+            {/* <Providers>{children}</Providers> */}
             <Toaster />
           </Suspense>
         </main>
       </body>
     </html>
-  )
+  );
 }
